4992668182baf083acac04113c38d00b5842e1a1
Yenny Delgado <yennyde0392@gmail.com>
2025-06-04 21:58:47 -0500

    Activity-category crud estable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ActivityCategoryService } from './activity-category/activity-category.service';
+import { ActivityCategoryService } from './modules/activity-category/activity-category.service';
+import { ActivityCategoryModule } from './modules/activity-category/activity-category.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ActivityCategoryModule],
   controllers: [AppController],
   providers: [AppService, ActivityCategoryService],
 })
